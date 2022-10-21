a8534b5c4fcec274ca11b041f2d07f134d2043d8
Spong3ibob <leonardomaran17@gmail.com>
2022-10-21 16:41:00 +0200

    aggiunto collegamento form a js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,19 @@
-
-const travel = travelKilometers * 0.25;
-console.log( "Prezzo intero" + " " + travel + "€");
-if(age <= 18){
-    resultjunior = travel - (travel * 0.2);
-    console.log( "Prezzo scontato junior" + " " + resultjunior + "€")
-}else if(age >= 65){
-    resultsenior = travel - (travel * 0.4);
-    console.log( "Prezzo scontato senior" + " " + resultsenior + "€")
-}
+let dati = document.forms.mydata;
+dati.addEventListener("submit", function() {
+    var textNome = document.getElementById("name")
+    var textKm = document.getElementById("km")
+})
+console.log(textNome);
+console.log(textKm);
+// const travel = travelKilometers * 0.25;
+// console.log( "Prezzo intero" + " " + travel + "€");
+// if(age <= 18){
+//     resultjunior = travel - (travel * 0.2);
+//     console.log( "Prezzo scontato junior" + " " + resultjunior + "€")
+// }else if(age >= 65){
+//     resultsenior = travel - (travel * 0.4);
+//     console.log( "Prezzo scontato senior" + " " + resultsenior + "€")
+// }
 
 
    
